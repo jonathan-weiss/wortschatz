e90f95ab58f744fefeeb7b252f4c9fdbf59c25d4
Jonathan Weiss <jonathan.weiss@ergon.ch>
2021-12-17 16:31:22 +0100

    Add open-api generator to have a specification first approach for REST interfaces (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.extensions.OpenApiGeneratorGenerateExtension
 
 plugins {
+    id("org.openapi.generator") version "5.3.0"
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.0"
@@ -36,4 +38,24 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named("compileKotlin") {
+    dependsOn ("openApiGenerate")
+}
+
+
+configure<OpenApiGeneratorGenerateExtension> {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/specs/petstore-v3.0.yaml")
+    outputDir.set("$buildDir/generated")
+    apiPackage.set("org.openapi.example.api")
+    invokerPackage.set("org.openapi.example.invoker")
+    modelPackage.set("org.openapi.example.model")
+    configOptions.set(
+        mapOf<String, String>(
+            "dateLibrary" to "java8"
+        )
+    )
+
 }
